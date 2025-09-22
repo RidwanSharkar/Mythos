00b436597094caee5fd1aceb7e1d4d2810a17743
Ridwan Sharkar <ridwansharkar@gmail.com>
2025-09-22 09:22:06 -0400

    anonymity requested by client

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,7 +86,7 @@ const GALLERY_ITEMS: GalleryItem[] = [
     description: 'Class: Runeblade\nElement: Frost\nAlignment: Chaotic Neutral',
     image: '/images/Frostbite.png',
     elements: ['Frost'],
-    price: 'Set by Christine Park'
+    price: 'Set by CP'
   },
   {
     id: 9,
